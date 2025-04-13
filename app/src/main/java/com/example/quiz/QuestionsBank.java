45f1e02b625805e53d86e74dce1334eac283c06e
Anahit <anahitqishmiryan49@gmail.com>
2025-04-13 11:58:29 +0400

    13.04

--- app/src/main/java/com/example/quiz/QuestionsBank.java
+++ app/src/main/java/com/example/quiz/QuestionsBank.java
@@ -7,9 +7,9 @@ public class QuestionsBank {
     private static List<QuestionsList> footballQuestions () {
         final List<QuestionsList> questionsList = new ArrayList<>();
         final QuestionsList question1 = new QuestionsList("Кто выиграл чемпионат мира по футболу в 2018 году?",
+                "Бразилия",
                 "Германия",
                 "Франция",
-                "Бразилия",
                 "Аргентина",
                 "Франция", "");
         final QuestionsList question2 = new QuestionsList("Сколько игроков на поле у одной команды в футболе (включая вратаря)?",
@@ -19,50 +19,50 @@ public class QuestionsBank {
                 "9",
                 "11", "");
         final QuestionsList question3 = new QuestionsList("Кто является лучшим бомбардиром Лиги чемпионов УЕФА?",
-                "Лионель Месси" ,
+                "Роберт Левандовски" ,
+                "Лионель Месси",
                 "Криштиану Роналду",
-                "Роберт Левандовски",
                 "Рауль",
                 "Криштиану Роналду", "");
         final QuestionsList question4 = new QuestionsList("Кто выиграл Золотой мяч в 2020 году?",
-                "Роберт Левандовски",
+                "Криштиану Роналду",
                 "Лионель Месси",
                 "Килиан Мбаппе",
-                "Криштиану Роналду",
+                "Роберт Левандовски",
                 "Роберт Левандовски", "");
         final QuestionsList question5 = new QuestionsList("Как называется знаменитый стадион Челси?",
+                "Сигнал Идуна Парк",
                 "Олд Траффорд",
                 "Стэмфорд Бридж",
-                "Сигнал Идуна Парк",
                 "Камп Ноу",
                 "Стэмфорд Бридж", "");
         final QuestionsList question6 = new QuestionsList("Какую команду называют Красные дьяволы?",
-                "Манчестер Сити",
                 "Манчестер Юнайтед",
+                "Манчестер Сити",
                 "Ливерпуль",
                 "Арсенал",
                 "Манчестер Юнайтед", "");
         final QuestionsList question7 = new QuestionsList("В какой стране прошел чемпионат мира по футболу в 2018 году?",
+                "Бразилия",
                 "Франция",
                 "Россия",
-                "Бразилия",
                 "Германия",
                 "Россия", "");
         final QuestionsList question8 = new QuestionsList("Какой клуб выиграл Лигу чемпионов в 2021 году?",
-                "Реал Мадрид",
-                "Манчестер Сити",
-                "Манчестер Юнайтед",
                 "Ливерпуль",
+                "Реал Мадрид",
+                "Манчестер Юнайтед",
+                "Манчестер Сити",
                 "Манчестер Сити", "");
         final QuestionsList question9 = new QuestionsList("Какую команду называют Королевский клуб?",
-                "Реал Мадрид",
                 "Манчестер Сити",
+                "Реал Мадрид",
                 "Ювентус",
                 "Ливерпуль",
                 "Реал Мадрид", "");
         final QuestionsList question10 = new QuestionsList("Кто является самым дорогим футболистом в истории на момент 2025 года?",
-                "Неймар",
                 "Килиан Мбаппе",
+                "Неймар",
                 "Эрлинг Холанд",
                 "Лионель Месси",
                 "Килиан Мбаппе", "");
@@ -82,66 +82,45 @@ public class QuestionsBank {
     }
     private static List<QuestionsList> mathQuestions () {
         final List<QuestionsList> questionsList = new ArrayList<>();
-        final QuestionsList question1 = new QuestionsList(".",
-                ".",
-                ".",
-                ".",
-                "1",
-                "1", "");
-        final QuestionsList question2 = new QuestionsList("",
-                ".",
-                ".",
-                ".",
-                "1",
-                "1", "");
-        final QuestionsList question3 = new QuestionsList("",
-                "1",
-                "2",
-                "3",
-                "4",
-                "2", "");
-        final QuestionsList question4 = new QuestionsList("",
-                "1",
-                "2",
-                "3",
-                "4",
-                "2", "");
-        final QuestionsList question5 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
+        final QuestionsList question1 = new QuestionsList("Чему равно значение числа пи (π) с точностью до двух десятичных знаков?",
+                "3.18", "3.16", "3.12", "3.14",
+                "3.14", "");
+        final QuestionsList question2 = new QuestionsList("Как называется операция, обратная умножению?",
+                "Деление", "Сложение", "Вычитание", "Возведение в степень",
+                "Деление", "");
+        final QuestionsList question3 = new QuestionsList("Какое из следующих чисел является простым числом?",
+                "24", "50", "37", "64",
+                "37", "");
+        final QuestionsList question4 = new QuestionsList("Чему равна сумма углов треугольника?",
+                "90°", "180°", "270°", "360°",
+                "180°", "");
+
+        final QuestionsList question5 = new QuestionsList("Как называется число, которое умножается само на себя?",
+                "Произведение", "Сумма", "Квадрат", "Степень",
+                "Квадрат", "");
+        final QuestionsList question6 = new QuestionsList("Сколько сторон имеет правильный семиугольник?",
+                "5", "7", "6", "8",
+                "7", "");
+        final QuestionsList question7 = new QuestionsList("Чему равен логарифм по основанию 10 числа 1000?",
+                "1", "2", "4", "3",
                 "3", "");
-        final QuestionsList question6 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
-        final QuestionsList question7 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
-        final QuestionsList question8 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
-        final QuestionsList question9 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
-        final QuestionsList question10 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
+        final QuestionsList question8 = new QuestionsList("Как называется теорема, утверждающая, что квадрат гипотенузы прямоугольного ",
+                "Теорема Ферма", "Теорема Пифагора",
+                "Теорема Архимеда",
+                "Теорема Эйлера",
+                "Теорема Пифагора", "");
+        final QuestionsList question9 = new QuestionsList("Как называется число, которое делится нацело на другое число?",
+                "Простое",
+                "Кратное",
+                "Составное",
+                "Целое",
+                "Кратное", "");
+        final QuestionsList question10 = new QuestionsList("Как называется операция, обратная извлечению квадратного корня?",
+                "Возведение в степень",
+                "Деление",
+                "Умножение",
+                "Возведение в квадрат",
+                "Возведение в квадрат", "");
 
         questionsList.add(question1);
         questionsList.add(question2);
@@ -158,66 +137,36 @@ public class QuestionsBank {
     }
     private static List<QuestionsList> countryQuestions () {
         final List<QuestionsList> questionsList = new ArrayList<>();
-        final QuestionsList question1 = new QuestionsList(".",
-                ".",
-                ".",
-                ".",
-                "1",
-                "1", "");
-        final QuestionsList question2 = new QuestionsList("",
-                ".",
-                ".",
-                ".",
-                "1",
-                "1", "");
-        final QuestionsList question3 = new QuestionsList("",
-                "1",
-                "2",
-                "3",
-                "4",
-                "2", "");
-        final QuestionsList question4 = new QuestionsList("",
-                "1",
-                "2",
-                "3",
-                "4",
-                "2", "");
-        final QuestionsList question5 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
-        final QuestionsList question6 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
-        final QuestionsList question7 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
-        final QuestionsList question8 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
-        final QuestionsList question9 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
-        final QuestionsList question10 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
+        final QuestionsList question1 = new QuestionsList("Какая страна является самой маленькой по площади среди независимых государств мира?",
+                "Ватикан", "Мальта", "США", "Науру",
+                "Ватикан", "");
+        final QuestionsList question2 = new QuestionsList("Какая страна является крупнейшим экспортером нефти в мире?",
+                "Россия", "Саудовская Аравия", "США", "Иран",
+                "Саудовская Аравия", "");
+        final QuestionsList question3 = new QuestionsList("Какая страна является крупнейшим производителем кофе в мире?",
+                "Бразилия", "Колумбия", "Эфиопия", "Вьетнам",
+                "Бразилия", "");
+        final QuestionsList question4 = new QuestionsList("Какая страна имеет самую длинную границу с водой в мире?",
+                "Канада", "Россия", "Индонезия", "Австралия",
+                "Россия", "");
+        final QuestionsList question5 = new QuestionsList("Какая страна является родиной самого высокого горного массива в мире, Гималаев?",
+                "Китай", "Непал", "Индия", "Бутан",
+                "Непал", "");
+        final QuestionsList question6 = new QuestionsList("В какой стране находится самый большой по площади национальный парк в мире, Восточно-Сибирский?",
+                "Канада", "Россия", "Австралия", "США",
+                "Россия", "");
+        final QuestionsList question7 = new QuestionsList("Какая страна является самой населенной в мире?",
+                "Китай", "Индия", "США", "Индонезия",
+                "Китай", "");
+        final QuestionsList question8 = new QuestionsList("В какой стране находится самое глубокое озеро в мире, Байкал?",
+                "Россия", "Канада", "США", "Казахстан",
+                "Россия", "");
+        final QuestionsList question9 = new QuestionsList("Какая страна является крупнейшим производителем бананов в мире?",
+                "Индия ", "Бразилия", "Эквадор", "Китай",
+                "Эквадор", "");
+        final QuestionsList question10 = new QuestionsList("Какая страна является самой большой по площади в мире?",
+                "Китай", "Канада", "Россия", "США",
+                "Китай", "");
 
         questionsList.add(question1);
         questionsList.add(question2);
@@ -234,66 +183,67 @@ public class QuestionsBank {
     }
     private static List<QuestionsList> factsQuestions () {
         final List<QuestionsList> questionsList = new ArrayList<>();
-        final QuestionsList question1 = new QuestionsList(".",
-                ".",
-                ".",
-                ".",
-                "1",
-                "1", "");
-        final QuestionsList question2 = new QuestionsList("",
-                ".",
-                ".",
-                ".",
-                "1",
-                "1", "");
-        final QuestionsList question3 = new QuestionsList("",
-                "1",
-                "2",
-                "3",
-                "4",
-                "2", "");
-        final QuestionsList question4 = new QuestionsList("",
-                "1",
-                "2",
-                "3",
-                "4",
-                "2", "");
-        final QuestionsList question5 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
-        final QuestionsList question6 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
-        final QuestionsList question7 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
-        final QuestionsList question8 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
-        final QuestionsList question9 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
-        final QuestionsList question10 = new QuestionsList("",
-                "1",
-                "4",
-                "2",
-                "3",
-                "3", "");
+        final QuestionsList question1 = new QuestionsList("Какая планета солнечной системы является самой горячей?",
+                "Венера",
+                "Марс",
+                "Меркурий",
+                "Юпитер",
+                "Венера", "");
+        final QuestionsList question2 = new QuestionsList("Какой материк является самым большим по площади?",
+                "Африка",
+                "Северная Америка",
+                "Евразия",
+                "Южная Америка",
+                "Евразия", "");
+        final QuestionsList question3 = new QuestionsList("Сколько костей взрослого человека содержит скелет?",
+                "Около 150",
+                "Около 200",
+                "Около 250",
+                "Около 300",
+                "Около 200", "");
+        final QuestionsList question4 = new QuestionsList("Какой океан является самым глубоким?",
+                "Тихий",
+                "Атлантический",
+                "Индийский",
+                "Южный",
+                "Тихий", "");
+        final QuestionsList question5 = new QuestionsList("Сколько континентов начинаются на букву 'A'?",
+                "Четыре",
+                "Два",
+                "Три",
+                "Один",
+                "Один", "");
+        final QuestionsList question6 = new QuestionsList("Какой самый крупный вид кошек?",
+                "Тигр",
+                "Ягуар",
+                "Лев",
+                "Пума",
+                "Лев", "");
+
+        final QuestionsList question7 = new QuestionsList("Какая самая высокая гора в Солнечной системе?",
+                "Эверест",
+                "Мауна-Кеа",
+                "Олимп",
+                "Вулкан Тау",
+                "Олимп", "");
+        final QuestionsList question8 = new QuestionsList("Сколько зубов в среднем у взрослого человека?",
+                "24",
+                "28",
+                "36",
+                "32",
+                "32", "");
+        final QuestionsList question9 = new QuestionsList("Какая планета солнечной системы имеет самую длинную орбиту?",
+                "Марс",
+                "Уран",
+                "Сатурн",
+                "Нептун",
+                "Уран", "");
+        final QuestionsList question10 = new QuestionsList("Какой металл является самым легким?",
+                "Железо",
+                "Титан",
+                "Алюминий",
+                "Медь",
+                "Алюминий", "");
 
         questionsList.add(question1);
         questionsList.add(question2);
